118452b631ce3d6943ff8e914861e3146d2e4c98
Punith Chavan <punithchavan05@gmail.com>
2025-06-22 12:48:43 +0530

    util code done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,18 @@ dotenv.config({
 
 
 connectDB()
-
+.then(()=>{
+    app.on("error", (error) => {
+        console.log("Error connecting to Express");
+        throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+.catch((error)=>{
+    console.log("Express connection failed", error);
+})
 
 
 
