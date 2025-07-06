e2b873983f5589cea3acbbb94237acf910957b39
Punith Chavan <punithchavan05@gmail.com>
2025-07-06 13:30:28 +0530

    add models for comments, likes, playlists, and tweets

---
+++ src/models/comment.model.js
@@ -0,0 +1,24 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSChema = new Schema({
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }
+
+}, {timestamps: true});
+
+commentSChema.plugin(mongooseAggregatePaginate);
+
+export const Comment = mongoose.model("Comment", commentSChema);
